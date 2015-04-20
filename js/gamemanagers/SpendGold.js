d520c76736c54904bdf0495cc2f32d034a33b8eb
Brandon Adonis Ortez <phoenixflower111@hotmail.com>
2015-04-20 15:02:21 -0700

    hi
    bye

--- js/gamemanagers/SpendGold.js
+++ js/gamemanagers/SpendGold.js
@@ -1,71 +1,3 @@
-game.GameTimerManager = Object.extend({
-    init: function(x, y, settings){
-        this.now = new Date().getTime();
-        this.lastCreep = new Date().getTime();
-        this.paused = false;
-        this.alwaysUpdate = true;
-    },
-    update: function(){
-        this.now = new Date().getTime();          
-        this.goldTimerCheck();
-        this.creepTimerCheck();
-        
-        return true;
-    },
-    goldTimerCheck: function(){
-        if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
-            game.data.gold += (game.data.exp1+1);
-            console.log("Current gold: " + game.data.gold);
-        }
-    },
-    creepTimerCheck: function(){
-        if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
-            this.lastCreep = this.now;
-            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-            me.game.world.addChild(creepe, 5);
-        }
-    }
-});
-
-game.HeroDeathManager = Object.extend({
-    init: function(x, y, settings){
-        this.alwaysUpdate = true;
-    },
-    update: function(){
-        if(game.data.player.dead){
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
-        
-        return true;
-    }
-});
-
-game.ExperienceManager = Object.extend({
-    init: function(x, y, settings){
-        this.alwaysUpdate = true;
-        this.gameover = false;
-    },
-    update: function(){
-        if(game.data.win === true && !this.gameover){
-            this.gameOver(true);
-        }else if(game.data.win === false && !this.gameover){
-           this.gameOver(false);
-        }
-        
-        return true;
-    },  
-    gameOver: function(win){
-        if(win){
-            game.data.exp += 10;
-        }else{
-            game.data.exp += 1;
-        }
-        this.gameover = true;
-        me.save.exp = game.data.exp;
-    }
-});
-
 game.SpendGold = Object.extend({
     init: function(x, y, settings){
         this.now = new Date().getTime();
@@ -109,7 +41,6 @@ game.SpendGold = Object.extend({
         me.input.bindKey(me.input.KEY.F6, "F6", true);
         this.setBuyText();
     }, 
-    
     setBuyText: function(){
          game.data.buytext = new (me.Renderable.extend({
                     init: function(){
@@ -201,23 +132,22 @@ game.SpendGold = Object.extend({
             game.data.playerAttack += 1;
         }else if(skill ===2){
             game.data.gold -= ((game.data.skill2 +1)* 10);
-            game.data.skill2 += 2;
+            game.data.skill2 += 1;
         }else if(skill ===3){
             game.data.gold -= ((game.data.skill3 +1)* 10);
-            game.data.skill3 += 3;
+            game.data.skill3 += 1;
         }else if(skill ===4){
             game.data.gold -= ((game.data.ability1 +1)* 10);
-            game.data.ability1 += 4;
+            game.data.ability1 += 1;
         }else if(skill ===5){
             game.data.gold -= ((game.data.ability2 +1)* 10);
-            game.data.ability2 += 5;
+            game.data.ability2 += 1;
         }else if(skill ===6){
             game.data.gold -= ((game.data.ability3 +1)* 10);
-            game.data.ability3 += 6;  
+            game.data.ability3 += 1;  
         }
     }
-    
+   
     
     
 });
-
---
+++ js/screens/newProfile.js
@@ -0,0 +1,36 @@
+game.newProfile = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('new-screen')), -10); // TODO
+
+                me.input.bindKey(me.input.KEY.F1, "F1");
+                me.input.bindKey(me.input.KEY.F2, "F2");
+                me.input.bindKey(me.input.KEY.F3, "F3");
+                me.input.bindKey(me.input.KEY.F4, "F4");
+                me.input.bindKey(me.input.KEY.F5, "F5");
+        
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [10, 10, 300, 50]);
+                        this.font = new me.Font("Arial", 26, "white");
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "PICK A USER NAME AND PASSWORD", this.pos.x, this.pos.y);
+                    }
+                   
+                })));
+           
+            
+        },
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+            
+	}
+});
