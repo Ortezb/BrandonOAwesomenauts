9e1199637ae52e67f68b7004c8592f6e1b6a3aee
Brandon Adonis Ortez <phoenixflower111@hotmail.com>
2015-04-21 15:02:24 -0700

    committing
    committing some more

--- js/screens/newProfile.js
+++ js/screens/newProfile.js
@@ -1,15 +1,15 @@
-game.newProfile = me.ScreenObject.extend({
+game.NewProfile = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('new-screen')), -10); // TODO
 
-                me.input.bindKey(me.input.KEY.F1, "F1");
-                me.input.bindKey(me.input.KEY.F2, "F2");
-                me.input.bindKey(me.input.KEY.F3, "F3");
-                me.input.bindKey(me.input.KEY.F4, "F4");
-                me.input.bindKey(me.input.KEY.F5, "F5");
+                me.input.unbindKey(me.input.KEY.B);
+                me.input.unbindKey(me.input.KEY.Q);
+                me.input.unbindKey(me.input.KEY.E);
+                me.input.unbindKey(me.input.KEY.W);
+                me.input.unbindKey(me.input.KEY.A);
         
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function(){
